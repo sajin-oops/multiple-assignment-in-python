918fdcb45d68fda99d5c4a581a038bcd7f6228dc
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-07-07 16:34:21 +0530

    Add files via upload

---
+++ multiple assignment.py
@@ -0,0 +1,15 @@
+name , age , height = "sajin" , 21 , "200"
+
+print(name)
+print(age)
+print(height)
+like = dislike =3400
+print(like)
+print(dislike)
+'''
+name = "sajin"
+age = 21
+height = 200
+print(height)
+'''
+
